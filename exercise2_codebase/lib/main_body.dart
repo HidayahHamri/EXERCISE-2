13fa55c1e0df56bf9cc7c6db4404820feca86bc1
Hidayah <nurhidayahhamri@gmail.com>
2021-11-29 18:14:53 +0800

    main body

--- exercise2_codebase/lib/main_body.dart
+++ exercise2_codebase/lib/main_body.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
 class MainBody extends StatelessWidget {
+  final _state;
+  MainBody({state}) : _state = state;
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Main Body'),
+      child: Column(children: [
+        Icon(Icons.access_alarm,
+            color: Color.fromRGBO(_state.red.round(), _state.green.round(),
+                _state.blue.round(), 1.0),
+            size: _state.size),
+      ]),
     );
   }
 }
